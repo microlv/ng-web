0d3cfd4ecd7ffccbf41709c06850522c89a6556f
lvjianyu <lvjianyu@live.com>
2014-11-03 17:14:51 +0800

    add angularjs&bootstrap&some lib

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -26,7 +27,7 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -37,7 +38,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -48,7 +49,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -56,5 +57,12 @@ app.use(function(err, req, res, next) {
     });
 });
 
+
+var debug = require('debug')('study-online');
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+});
 
 module.exports = app;
---
+++ public/javascripts/study-online-app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Andy.Lv on 2014/11/3.
+ */
+
+(function (angular) {
+    'use strict';
+
+    angular.module('studyOnlineApp', [function () {
+
+    }]);
+
+})(angular);
+
