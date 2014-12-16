6f1900af04094404975ff84df925d0b37d3a8320
lvjianyu <lvjianyu@live.com>
2014-12-16 23:06:21 +0800

    remove router js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,15 @@
 /**
  * Created by Andy.Lv on 2014/11/13.
  */
-var layout = require('./layout');
-var templates = require('./templates');
-var partials = require('./partials');
+//var layout = require('./layout');
+//var templates = require('./templates');
+//var partials = require('./partials');
+
+var router = require('./router_api');
 
 module.exports = function (app) {
-    app.use('/', layout.index);
-    app.use('/layout', layout.layout);
-    app.use('/templates', templates);
-    app.use('/partials', partials);
+    app.use('/', router.index);
+    app.use('/layout', router.layout);
+    app.use('/templates', router.templates);
+    app.use('/partials', router.partials);
 };
