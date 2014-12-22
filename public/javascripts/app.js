e90380622df41393dc3656033c99d3b92b3958c3
andy.lv <andy.lv@live.com>
2014-12-22 23:39:08 +0800

    move the platform js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,40 +11,41 @@
 		'ui.bootstrap',
 		'platform',
 		'authentication',
-		'edu'
+		'edu',
+		'ngMessage'
 	];
 
 	var app = angular.module('app', dependInject);
 
 	//app.config(['$provide', function ($provide) {
-	//    $provide.decorator('$templateCache', ['$http', '$delegate', function ($http, $delegate) {
-	//        $delegate.loadedTemplateUrl = function (url) {
-	//            $http({
-	//                url: url,
-	//                method: 'GET'
-	//            }).then(function (r) {
-	//                _.forEach(r.data, function (k) {
-	//                    $delegate.put(k);
-	//                });
-	//            });
-	//        };
-	//    }]);
+	//	$provide.decorator('$templateCache', ['$http', '$delegate', function ($http, $delegate) {
+	//		$delegate.loadedTemplateUrl = function (url) {
+	//			$http({
+	//				url: url,
+	//				method: 'GET'
+	//			}).then(function (r) {
+	//				_.forEach(r.data, function (k) {
+	//					$delegate.put(k);
+	//				});
+	//			});
+	//		};
+	//	}]);
 	//}]);
-	//
-	app.run(['$templateCache', '$http', '$compile', function ($templateCache, $http, $compile) {
-		//$http({
-		//    url: 'layout/layout-template',
-		//    method: 'GET'
-		//}).then(function (r) {
-		//    $compile(r.data);
-		//
-		//    //_.forEach(r.data, function (k) {
-		//    //    $templateCache.put(k);
-		//    //});
-		//});
 
-		//$templateCache.loadedTemplateUrl('/');
-	}]);
+	//app.run(['$templateCache', '$http', '$compile', function ($templateCache, $http, $compile) {
+	//	//$http({
+	//	//    url: 'layout/layout-template',
+	//	//    method: 'GET'
+	//	//}).then(function (r) {
+	//	//    $compile(r.data);
+	//	//
+	//	//    //_.forEach(r.data, function (k) {
+	//	//    //    $templateCache.put(k);
+	//	//    //});
+	//	//});
+	//	//
+	//	//$templateCache.loadedTemplateUrl('/');
+	//}]);
 
 
 })(angular);
