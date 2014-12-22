ade11fbf5c287ce7f18e17b62d7a1a5f4ce92db5
andy.lv <andy.lv@live.com>
2014-12-22 21:56:04 +0800

    heroku push v1

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var web_api = require('./web_api');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
+app.engine('html', require('ejs').__express);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+web_api(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
