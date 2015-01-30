4de42d43354e9ff6bbffa5da5bc0b21d3ea0e448
andy.lv <andy.lv@live.com>
2015-01-30 23:00:44 +0800

    make publish app work

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ module.exports = {
     dbconnect: 'mongodb://127.0.0.1/ng-web',
     session_secret: 'ng-web',
 
-
+    MONGOLAB_URI: 'MONGOLAB_URI',
+    GITHUB_CLIENT_ID: 'GITHUB_CLIENT_ID',
+    GITHUB_CLIENT_SECRET: 'GITHUB_CLIENT_SECRET',
+    GITHUB_CALLBACK: 'GITHUB_CALLBACK'
 };
