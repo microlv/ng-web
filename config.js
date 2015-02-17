bff86185fe65d99cbc988db4285a37dec09e16ef
andy.lv <andy.lv@live.com>
2015-02-17 11:27:02 +0800

    minify css/js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ module.exports = {
     session_secret: 'ng-web',
     auth_cookie_name: 'ng-web',
 
-    MONGOLAB_URI: 'MONGOLAB_URI',
-    GITHUB_CLIENT_ID: 'GITHUB_CLIENT_ID',
-    GITHUB_CLIENT_SECRET: 'GITHUB_CLIENT_SECRET',
-    GITHUB_CALLBACK: 'GITHUB_CALLBACK'
+    //MONGOLAB_URI: 'mongodb://heroku_app33118649:96tbme73efvbvv1b6k9hacalf0@ds031681.mongolab.com:31681/heroku_app33118649',
+    GITHUB_CLIENT_ID: '189974e64f55c71a791d',
+    GITHUB_CLIENT_SECRET: 'a9bc4ec84e3e10995e22df3925d62974fb0bd244',
+    GITHUB_CALLBACK: 'http://localhost:3000/api/auth/github/callback'
     //ROLLBAR_ACCESS_TOKEN: ''
 };
