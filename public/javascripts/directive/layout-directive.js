82e163bc8aacb8663ee7f1b330afd582bfe08d84
andy.lv <andy.lv@live.com>
2015-02-26 23:23:08 +0800

    min js

--- public/javascripts/directive/layout-directive.js
+++ public/javascripts/directive/layout-directive.js
@@ -5,49 +5,45 @@
 (function (angular) {
     'use strict';
 
-    angular.module('ngWeb')
-        .directive('layoutHeader', ['$templateCache', 'dialogService', function ($templateCache, dialogService) {
-            return {
-                restrict: 'EA',
-                template: $templateCache.get('layout-header.html'),
-                controller: function ($scope) {
-                    $scope.login = function () {
-                        dialogService.show({
-                            template: 'login-template.html'
-                        });
-                    };
-                }
-            };
-        }])
-        .directive('layoutFooter', ['$templateCache', function ($templateCache) {
-            return {
-                restrict: 'EA',
-                template: $templateCache.get('layout-footer.html')
-            };
-        }])
-        .directive('layoutSidebar', ['$templateCache', '$state', 'topicItemSource', function ($templateCache, $state, topicItemSource) {
-            return {
-                restrict: 'EA',
-                template: $templateCache.get('layout-sidebar.html'),
-                link: function (scope, ele, att) {
-                    _.forEach(topicItemSource, function (k) {
-                        k.href = $state.href('category', {category: k.uiSref});
+    //use angular.module('ngWeb') because when uglify js, will exception.
+    angular.module('ngWeb').directive('layoutHeader', ['$templateCache', 'dialogService', function ($templateCache, dialogService) {
+        return {
+            restrict: 'EA',
+            template: $templateCache.get('layout-header.html'),
+            controller: ['$scope', function ($scope) {
+                $scope.login = function () {
+                    dialogService.show({
+                        template: 'login-template.html'
                     });
-                    scope.layoutSidebarItemSource = topicItemSource;
-                }
-            };
-        }])
-        .directive('layoutContent', ['$templateCache', function ($templateCache) {
-            return {
-                restrict: 'EA',
-                template: $templateCache.get('layout-content.html')
-            };
-        }])
-        .directive('layoutSearch', ['$templateCache', function ($templateCache) {
-            return {
-                restrict: 'EA',
-                template: $templateCache.get('layout-search.html')
-            };
-        }]);
+                };
+            }]
+        };
+    }]).directive('layoutFooter', ['$templateCache', function ($templateCache) {
+        return {
+            restrict: 'EA',
+            template: $templateCache.get('layout-footer.html')
+        };
+    }]).directive('layoutSidebar', ['$templateCache', '$state', 'topicItemSource', function ($templateCache, $state, topicItemSource) {
+        return {
+            restrict: 'EA',
+            template: $templateCache.get('layout-sidebar.html'),
+            link: function (scope, ele, att) {
+                _.forEach(topicItemSource, function (k) {
+                    k.href = $state.href('category', {category: k.uiSref});
+                });
+                scope.layoutSidebarItemSource = topicItemSource;
+            }
+        };
+    }]).directive('layoutContent', ['$templateCache', function ($templateCache) {
+        return {
+            restrict: 'EA',
+            template: $templateCache.get('layout-content.html')
+        };
+    }]).directive('layoutSearch', ['$templateCache', function ($templateCache) {
+        return {
+            restrict: 'EA',
+            template: $templateCache.get('layout-search.html')
+        };
+    }]);
 
 })(angular);
