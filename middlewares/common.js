f9ed9d1b8f86ee2fcadaee4044ebc7bb12f4947f
microlv <andy.lv@live.com>
2015-03-19 08:38:47 +0800

    changed to use http

--- middlewares/common.js
+++ middlewares/common.js
@@ -5,7 +5,12 @@
 
 function requireHttps() {
   return function (req, res, next) {
-    //because cdn can't work, remove it temp.
+    //because cdn can't work, remove it use http temp.
+    if (process.env.requireHttps && req.headers['x-forwarded-proto'] !== 'http') {
+      console.log('require http');
+      res.redirect('http://' + req.headers.host + req.path);
+    }
+    
     //if (process.env.requireHttps && req.headers['x-forwarded-proto'] !== 'https') {
     //  console.log('require https');
     //  res.redirect('https://' + req.headers.host + req.path);
